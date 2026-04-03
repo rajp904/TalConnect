f5191558204f57cc44abdae1dff2f0b5109b7c6a
rajp904 <princesingh17364@gmail.com>
2026-04-03 22:52:02 +0530

    fixed cors for deploy

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const flash = require("connect-flash");
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://talconnect.vercel.app"],
   credentials: true
 }));
 
